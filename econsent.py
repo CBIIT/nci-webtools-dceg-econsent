1c87559a57264e1291ff759789b36e9eab6cc324
scott75219 <scott.goldweber@gmail.com>
2016-07-11 16:41:29 -0400

    wsgi

--- econsent.py
+++ econsent.py
@@ -118,5 +118,5 @@ def after_request(response):
     return response
 
 
-if __name__ == "__main__":
-  app.run(host = '0.0.0.0', port = 1111, debug = True)
+#if __name__ == "__main__":
+#  app.run(host = '0.0.0.0', port = 1111, debug = True)
